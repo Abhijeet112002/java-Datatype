945ad6702644895559035ccd1135a0ceec1f1985
ABHIJEET GHORPADE <you@example.com>
2024-10-16 20:48:54 +0530

    first-commit

---
+++ OneDrive/Desktop/java-Datatype/Datatype.java
@@ -0,0 +1,200 @@
+public class Datatype{
+	public static void main(String[] args){
+
+		byte petrolPrice = 103;
+		byte temperatureInCel = 100;
+		byte intrestRate = 15;
+		byte onionPerkg = 80;
+		byte hoursInADay = 24;
+		byte daysInWeek = 7;
+		byte valueOfOneByte = 8;
+		byte phValue = 14;
+		byte semInEng = 8;
+		byte numberOfDistrictInKa = 30;
+
+
+		System.out.println(petrolPrice);
+		System.out.println(temperatureInCel);
+		System.out.println(intrestRate);
+		System.out.println(onionPerkg);
+		System.out.println(hoursInADay);
+		System.out.println(daysInWeek );
+		System.out.println(valueOfOneByte);
+		System.out.println(phValue);
+		System.out.println(semInEng);
+		System.out.println(numberOfDistrictInKa);
+
+
+		short daysInYear = 365;
+		short articleinIndia = 470;
+		short riverInIndia = 400;
+		short currentYear = 2024;
+		short numberOfMpInindia = 545;
+		short gymfees = 1000;
+		short independeceYear = 1947;
+		short numberOfDamsInIndia = 5745;
+		short height = 170;
+		short turtleLifespan = 250;
+
+
+		System.out.println(daysInYear); 
+		System.out.println(articleinIndia); 
+		System.out.println(riverInIndia); 
+		System.out.println(currentYear); 
+		System.out.println( numberOfMpInindia); 
+		System.out.println(gymfees); 
+		System.out.println(independeceYear); 
+		System.out.println(numberOfDamsInIndia); 
+		System.out.println(height); 
+		System.out.println(turtleLifespan);
+
+
+		int bikeRate = 2000000;
+		int studentsInUniversity = 500000;
+		int otp = 123456;
+		int laptopPrice = 6000000;
+		int luxuryRoomRent = 100000;
+		int collegeFees = 400000;
+		int pincode = 123456;
+		int iphonePrice = 150000;
+		int tharPrice = 250000;
+		int weightOfBlueWhale = 330000;
+
+		System.out.println(bikeRate); 
+		System.out.println(studentsInUniversity); 
+		System.out.println(otp); 
+		System.out.println(laptopPrice); 
+		System.out.println(luxuryRoomRent );
+		System.out.println(collegeFees);
+		System.out.println(pincode); 
+		System.out.println(iphonePrice);
+		System.out.println(tharPrice ); 
+		System.out.println(weightOfBlueWhale); 
+
+
+		long mobileNumber = 9632662832l;
+		long aadharNumber = 830060700832l;
+		long accountNumber = 64149352708568l;
+		long registrationNumber = 12345678956l;
+		long bankbalance = 6234726466731l;
+		long ambaniTotalWorth = 43672487312474l;
+		long mobileSerialNumber = 7899384392472l;
+		long sheepPrice = 37248324992343l;
+		long tataWorth = 6733284693794l;
+		long areaOfindia = 87324637198746l;
+
+		System.out.println(mobileNumber );
+		System.out.println(aadharNumber);
+		System.out.println(accountNumber);
+		System.out.println( registrationNumber);
+		System.out.println(bankbalance );
+		System.out.println(ambaniTotalWorth);
+		System.out.println(mobileSerialNumber);
+		System.out.println(sheepPrice);
+		System.out.println(tataWorth);
+		System.out.println(areaOfindia);
+
+		float pieValue = 3.14f;
+		float weight = 67.784f;
+		float heights = 175.56f;
+		float airtelPack = 1.5f;
+		float speedOflibht = 3.10f;
+		float speedOfAir = 12.8f;
+		float roomTemperature = 33.7f;
+		float tax = 3.4f;
+		float intrestRates = 7.5f;
+		float price = 45.789f;
+
+
+		System.out.println(pieValue);
+		System.out.println(weight);
+		System.out.println(heights);
+		System.out.println(airtelPack );
+		System.out.println(speedOflibht);
+		System.out.println(speedOfAir );
+		System.out.println(roomTemperature);
+		System.out.println(tax);
+		System.out.println(intrestRates);
+		System.out.println(price);
+
+
+		double discount = 50.3838d;
+		double time = 2.342d;
+		double exponent = 3.490d;
+		double totalMinutes = 145.373d;
+		double netSalary = 454.3478321d;
+		double dividend = 54.464d;
+		double divisor = 436.387d;
+		double taxes = 37.329d;
+		double range = 35.378d;
+		double seconds = 36.3287d;
+
+
+		System.out.println(discount);
+		System.out.println(time);
+		System.out.println(exponent);
+		System.out.println(totalMinutes);
+		System.out.println(netSalary);
+		System.out.println(dividend );
+		System.out.println(divisor);
+		System.out.println(taxes);
+		System.out.println(range);
+		System.out.println(seconds);
+
+
+		char section = 'a';
+		char division = 'g';
+		char batch = 'a';
+		char jobClass = 'd';
+		char powerUnit = 'w';
+		char distanceUnit = 'm';
+		char timing = 's';
+		char bold = 'b';
+		char italic = 'i';
+		char function = 'f';
+
+
+		System.out.println(section);
+		System.out.println(division);
+		System.out.println(batch);
+		System.out.println(jobClass);
+		System.out.println(powerUnit);
+		System.out.println(distanceUnit);
+		System.out.println(timing);
+		System.out.println(bold);
+		System.out.println(italic);
+		System.out.println(function);
+
+
+		boolean abdulKalamIsMissaileMan = true;
+		boolean tajHotelOwnsByTataGroup = true;
+		boolean isTodayHoliday = false;
+		boolean isTommorowHoliday = true;
+		boolean isModiOurPM = true;
+		boolean indiaIsState = false;
+		boolean youAreSmart = false;
+		boolean areYouInSpace = false;
+		boolean areYouYoutuber = true;
+		boolean relianceOwnedByAmbani = true;
+
+		System.out.println(abdulKalamIsMissaileMan);
+		System.out.println(tajHotelOwnsByTataGroup);
+		System.out.println(isTodayHoliday);
+		System.out.println(isTommorowHoliday);
+		System.out.println(isModiOurPM);
+		System.out.println(indiaIsState);
+		System.out.println(youAreSmart );
+		System.out.println(areYouInSpace);
+		System.out.println(areYouInSpace);
+		System.out.println(areYouYoutuber);
+		System.out.println(relianceOwnedByAmbani);
+		
+		
+		
+
+		
+
+	
+
+	}
+}
